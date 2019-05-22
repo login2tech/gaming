155a91bcf5e6bcbe759b5d55e8c5292fc51fc943
Vasu Chawla <vchawla26@gmail.com>
2019-05-23 00:05:31 +0530

    fix upvote

--- app/components/Modules/UpvoteButton.js
+++ app/components/Modules/UpvoteButton.js
@@ -4,24 +4,28 @@ import {upVote, downVote} from '../../actions/social';
 
 class UpvoteButton extends React.Component {
   state = {changed: false, doChange: 0};
-  changeVote(already_voted, e) {
-    if (already_voted) {
+  changeVote(already_voted, e, type, old_type) {
+    if (old_type && old_type == type) {
       this.handleDownVote(e);
     } else {
-      this.handleUpVote(e);
+      this.handleDownVote(e);
+      setTimeout(() => {
+        this.handleUpVote(e, type);
+      }, 500);
     }
   }
 
-  handleUpVote(e) {
+  handleUpVote(e, type) {
     this.props.dispatch(
       upVote(
         {
-          post_id: this.props.post_id
+          post_id: this.props.post_id,
+          type: type
         },
         res => {
           if (res) {
             this.setState({
-              changed: !this.state.changed,
+              changed: type,
               doChange: this.state.doChange + 1
             });
           }
@@ -38,11 +42,12 @@ class UpvoteButton extends React.Component {
       downVote(
         {
           post_id: this.props.post_id
+          // type:this
         },
         res => {
           if (res) {
             this.setState({
-              changed: !this.state.changed,
+              changed: 'NO',
               doChange: this.state.doChange - 1
             });
           }
@@ -58,45 +63,91 @@ class UpvoteButton extends React.Component {
     }
 
     let btn_class = 'btn btn-sm btn-vote pulsate-fwd action-upvote ';
-    btn_class += voted ? ' upvoted ' : '';
+    // btn_class += voted ? ' upvoted ' : '';
     btn_class += 'btn_4_ques_' + this.props.post_id;
     btn_class += this.props.disabled ? ' disabled ' : '';
+    const emojis = [
+      {key: 4, em: 'like'},
+      {key: 3, em: 'heart'},
+      {key: 2, em: 'angry'},
+      {key: 1, em: '100'}
+    ];
+    const like_counts = {};
+    let i_have_liked = false;
+    let original_liked = false;
+    const {likes} = this.props;
+    for (let i = 0; i < likes.length; i++) {
+      if (!like_counts[likes[i].type]) {
+        like_counts[likes[i].type] = 1;
+      } else {
+        like_counts[likes[i].type]++;
+      }
+      if (likes[i].user_id == this.props.me) {
+        i_have_liked = likes[i].type;
+        original_liked = likes[i].type;
+      }
+    }
+    if (this.state.changed) {
+      i_have_liked = this.state.changed;
+    }
+    if (i_have_liked == 'NO') {
+      i_have_liked = false;
+    }
+    if (original_liked === false && i_have_liked !== false) {
+      like_counts[i_have_liked]++;
+    }
+    if (original_liked && i_have_liked != original_liked) {
+      like_counts[original_liked]--;
+      if (like_counts[original_liked] < 1) {
+        like_counts[original_liked] = 0;
+      }
+      like_counts[i_have_liked]++;
 
+      // like_counts[likes[i].type]++;
+    }
     return (
-      <button
-        type="button"
-        className={btn_class}
-        data-id={this.props.post_id}
-        onClick={e => {
-          if (this.props.disabled) {
-            return;
-          }
-          this.changeVote(voted, e);
-        }}
-      >
-        <img
-          src="/images/controller.svg"
-          style={{
-            width: '20px',
-            marginRight: 5
-          }}
-        />
-        <span className="hidden-sm-down">
-          {voted ? 'Liked' : 'Like'}
-          <span>
-            {' '}
-            (
-            {this.state.changed
-              ? this.props.total_count
-                ? this.props.total_count + this.state.doChange
-                : '1'
-              : this.props.total_count
-                ? this.props.total_count
-                : '0'}
-            )
-          </span>
-        </span>
-      </button>
+      <span>
+        {emojis.map((emoji, i) => {
+          return (
+            <button
+              style={{marginRight: '5px'}}
+              key={emoji.key}
+              type="button"
+              className={
+                btn_class +
+                (i_have_liked && i_have_liked == emoji.em ? ' upvoted ' : '   ')
+              }
+              data-id={this.props.post_id}
+              onClick={e => {
+                if (this.props.disabled) {
+                  return;
+                }
+                this.changeVote(voted, e, emoji.em, i_have_liked);
+              }}
+            >
+              <img
+                src={'/images/emoji/emoji_' + emoji.em + '.png'}
+                style={{
+                  width: '20px',
+                  marginRight: 5
+                }}
+              />
+              <span className="hidden-sm-down">
+                <span>
+                  ({like_counts[emoji.em] ? like_counts[emoji.em] : '0'})
+                  {/* {this.state.changed
+                    ? this.props.total_count
+                      ? this.props.total_count + this.state.doChange
+                      : '1'
+                    : this.props.total_count
+                      ? this.props.total_count
+                      : '0'} */}
+                </span>
+              </span>
+            </button>
+          );
+        })}
+      </span>
     );
   }
 }
