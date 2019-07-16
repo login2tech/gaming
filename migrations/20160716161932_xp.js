b1f156a04b8676e80dcd9422c6c1a5cb91305635
Vasu Chawla <vchawla26@gmail.com>
2019-07-17 00:11:19 +0530

    ert

---
+++ migrations/20160716161932_xp.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('xp', function(table) {
+      table.increments();
+      table.integer('user_id').references('users.id');
+      table.integer('season');
+      table.integer('year');
+      table.integer('xp');
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+//
+};
---
+++ models/XP.js
@@ -0,0 +1,12 @@
+const bookshelf = require('../config/bookshelf');
+const User = require('../models/User');
+
+const XP = bookshelf.Model.extend({
+  tableName: 'xp',
+  hasTimestamps: false,
+  user: function() {
+    return this.belongsTo(User);
+  }
+});
+
+module.exports = XP;
