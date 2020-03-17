ed9dfe74b6e26665eba0ee71788a07ece2c31a64
Vasu Chawla <vchawla26@gmail.com>
2020-03-17 11:41:19 +0530

    fix

--- app/components/Modules/HeaderBox.js
+++ app/components/Modules/HeaderBox.js
@@ -1,19 +1,25 @@
 import React from 'react';
 
-export default  HeaderBox = props => {
-  console.log(props);
-  return (
-    <section className="page_title_bar noblend">
-      <div className="container-fluid half">
-        <div className="row">
-          <div className="col-md-12 col-sm-12 col-xs-12">
-            <div className="section-headline white-headline text-left">
-              <h3 className="no-case-change  lil-small">{props.title}</h3>
-              <br />
+
+
+class Header extends React.Component {
+
+  render(){
+    return (
+      <section className="page_title_bar noblend">
+        <div className="container-fluid half">
+          <div className="row">
+            <div className="col-md-12 col-sm-12 col-xs-12">
+              <div className="section-headline white-headline text-left">
+                <h3 className="no-case-change  lil-small">{this.props.title}</h3>
+                <br />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  );
-};
+      </section>
+    );
+  }
+  
+}
+export default Header;
