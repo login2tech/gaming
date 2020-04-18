7bb25652e8df9d722bf92d9d462ebc2ee2835b0d
Vasu Chawla <vchawla26@gmail.com>
2020-04-18 09:15:35 +0530

    fixes

--- controllers/credits.js
+++ controllers/credits.js
@@ -656,7 +656,7 @@ const create_new_user_for_token = function(req, res, next) {
     {
       description: 'Created for OnlyCompGaming',
       email: req.user.email,
-      nane: req.user.first_name + ' ' + req.user.last_name,
+      name: req.user.first_name + ' ' + req.user.last_name,
       source: stripeToken
     },
     function(err, customer) {
